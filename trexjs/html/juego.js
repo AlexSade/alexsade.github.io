1bd6a97b357c283af6d32d4194a45fd472b59dbb
Alejandro Perez <alxpefa@gmail.com>
2020-05-22 17:22:56 +0200

    elementos pasados a arrray

--- trexjs/html/juego.js
+++ trexjs/html/juego.js
@@ -1,5 +1,5 @@
 document.addEventListener('keydown', function (evento) {
-  if (evento.keyCode == 32 && trex.saltando == false) {
+  if (evento.keyCode == 32 && elements[0].saltando == false) {
     console.log('salta');
     saltar();
   }
@@ -12,9 +12,9 @@ var fps = 60;
 function reinicio(){
   nivel.muerto = false;
   nivel.puntuacion = 0;
-  cactus.x = ancho + 100;
+  elements[1].x = ancho + 100;
   nube.x = ancho + 100;
-  trex.y = suelo;
+  elements[0].y = suelo;
   nivel.nivel = 0;
   nivel.velocidad = 7;
 }
@@ -74,16 +74,7 @@ function borrar() {
 }
 
 var suelo = alto - 60;
-
-var trex = {
-  y: suelo,
-  velY: 0,
-  gravedad: 2,
-  salto: 28,
-  saltando: false,
-
-};
-
+  
 var nivel = {
   nivel: 0,
   velocidad: 7,
@@ -91,25 +82,45 @@ var nivel = {
   muerto: false,
 };
 
-var cactus = {
-  x: ancho + 100,
-  y: suelo - 25,
-};
-
 var sueloG = {
   x : 0,
   y : alto - 30,
 };
 
+  
 var nube = {
   x : ancho + 100,
   y : 50,
 };
 
-var pajaro = {
-  x : ancho + 100,
-  y : 50
-};
+var elements = [
+  trex = {
+    img: imgRex1,
+    y: suelo,
+    velY: 0,
+    gravedad: 2,
+    salto: 28,
+    saltando: false,
+    sizeX: 0,
+    sizeY: 0
+  },
+
+  cactus = {
+    x: ancho + 100,
+    y: suelo - 25,
+    sizeX: 0,
+    sizeY: 0
+  },
+  
+  pajaro = {
+    img: imgPajaro1,
+    x : ancho + 100,
+    y : 50,
+    sizeX: 0,
+    sizeY: 0
+  }
+
+];
 
 //FUNCIONES SUELO
 function dibujaSuelo() {
@@ -122,19 +133,27 @@ function logicaSuelo(){
     sueloG.x -= nivel.velocidad;
   }
 }
+
 //FUNCIONES TREX
 var andar = false;
 
 function dibujaRex() {
   
-  if(trex.saltando == true){
-    ctx.drawImage(imgRex, 0, 0, 480, 504, 100, trex.y, 50, 50);
+  //Elige la imagen a mostrar según la situacion del trex
+  //y establece su tamaño impreso
+  if(elements[0].saltando == true){
+    elements[0].img = imgRex;
+    elements[0].sizeX = 50;
+    elements[0].sizeY = 50;
   }else if(andar == true){
-    ctx.drawImage(imgRex1, 0, 0, 480, 504, 100, trex.y, 50, 50);
+    elements[0].img = imgRex1;
+    elements[0].sizeX = 50;
+    elements[0].sizeY = 50;
   }else {
-    ctx.drawImage(imgRex2, 0, 0, 480, 504, 100, trex.y, 50, 50);
+    elements[0].img = imgRex2;
+    elements[0].sizeX = 50;
+    elements[0].sizeY = 50;
   }
-
 
   if (acum == 10){
     if(andar == false){
@@ -144,37 +163,40 @@ function dibujaRex() {
     }
     acum = 0;
   }
+
+  //Lo dibuja en el canvas
+  ctx.drawImage(elements[0].img, 0, 0, 480, 504, 100, elements[0].y, elements[0].sizeX, elements[0].sizeY);
 }
 
 function saltar() {
-  trex.saltando = true;
-  trex.velY = trex.salto;
+  elements[0].saltando = true;
+  elements[0].velY = elements[0].salto;
 }
 
 function gravedad() {
-  if (trex.saltando == true) {
-    if (trex.y - trex.velY - trex.gravedad > suelo) {
-      trex.saltando = false;
-      trex.velY = 0;
-      trex.y = suelo;
+  if (elements[0].saltando == true) {
+    if (elements[0].y - elements[0].velY - elements[0].gravedad > suelo) {
+      elements[0].saltando = false;
+      elements[0].velY = 0;
+      elements[0].y = suelo;
     } else {
-      trex.velY -= trex.gravedad;
-      trex.y -= trex.velY;
+      elements[0].velY -= elements[0].gravedad;
+      elements[0].y -= elements[0].velY;
     }
   }
 }
 
 //FUNCIONES CACTUS
 function dibujarCactus(){
-  ctx.drawImage(imgCactus, 0, 0, 169, 276, cactus.x, cactus.y, 38, 75);
+  ctx.drawImage(imgCactus, 0, 0, 169, 276, elements[1].x, elements[1].y, 38, 75);
 }
 
 function logicaCactus(){
-  if(cactus.x < -100){
-    cactus.x = ancho + 100;
+  if(elements[1].x < -100){
+    elements[1].x = ancho + 100;
     nivelAument();
   }else{
-    cactus.x -= nivel.velocidad;
+    elements[1].x -= nivel.velocidad;
   }
 }
 
@@ -193,21 +215,30 @@ function logicaNube(){
 
 //FUNCIONES PAJARO
 var aleteo = false;
+
 function dibujaPajaro(){
+  //Elige imagen a mostrar y establece sus medidas
   if(aleteo){
-    ctx.drawImage(imgPajaro1, 0, 0, 252, 156, pajaro.x, pajaro.y, 108, 44);
+    elements[2].img = imgPajaro1;
+    elements[2].sizeY = 44;
+    elements[2].sizeX = 108;
   }else{
-    ctx.drawImage(imgPajaro2, 0, 0, 252, 156, pajaro.x, pajaro.y, 108, 44);
+    elements[2].img = imgPajaro2;
+    elements[2].sizeY = 44;
+    elements[2].sizeX = 108;
   }
+  //Dibuja el pajaro
+  ctx.drawImage(pajaro.img, 0, 0, 252, 156, elements[2].x, elements[2].y, elements[2].sizeX, elements[2].sizeY);
   
 }
 
 function logicaPajaro(){
-  if(pajaro.x < -100){
-    pajaro.x = ancho + 100;
+  if(elements[2].x < -100){
+    elements[2].x = ancho + 100;
   }else{
-    pajaro.x  -= nivel.velocidad / 2;
+    elements[2].x  -= nivel.velocidad / 2;
   }
+
   if(acumPajaro == 20){
     if(aleteo){
       aleteo = false;
@@ -220,10 +251,10 @@ function logicaPajaro(){
 
 //FUNCION COLISION
 function colision(){
-  if(cactus.x >= 62 && cactus.x <= 150){
-    if(trex.y >= cactus.y - 20){
+  if(elements[1].x >= 62 && elements[1].x <= 150){
+    if(elements[0].y >= elements[1].y - 20){
       nivel.muerto = true;
-      trex.saltando = false;
+      elements[0].saltando = false;
     }
     nivel.puntuacion += 10;
     console.log(nivel.puntuacion);
@@ -251,8 +282,8 @@ function imprimir(){
     ctx.fillText("GAME OVER",240,150);
   }
 }
-//BUCLE PRINCIPAL
 
+//BUCLE PRINCIPAL
 var acum = 0;
 var acumPajaro = 0;
 
